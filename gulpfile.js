ff17e04d51fba7cf83a517860678bcf9b62b4616
Guebes <gabriellbermond@hotmail.com>
2026-02-13 17:27:48 -0300

    Update of all archives

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const pkgGulp = require('gulp');
 const pkgSass_GulpSass = require('gulp-sass')(require('sass'))
+const imgMin = require('gulp-imagemin')
 
 //1 - Função para compilar o Sass
 function SassCss(){
@@ -13,6 +14,14 @@ function SassCss(){
     .pipe(pkgGulp.dest('./build/styles'))
 }
 
+function minImg(){
+    return pkgGulp.src('./Img/**/**/**/*')
+    .pipe(imgMin({
+        outputStyle:'compressed'
+    }))
+    .pipe(pkgGulp.dest('./build/CompImg'))
+}
+
 function observar(){
     //watch() precisa de dois argumentos, sendo eles o caminho do(s) arquivo(s) que vai observar e a função que vai ser disparada
     pkgGulp.watch('./source/styles/*.scss', pkgGulp.parallel(SassCss))
@@ -20,3 +29,4 @@ function observar(){
 
 exports.default = SassCss;
 exports.watch = observar;
+exports.compImgs = minImg;
